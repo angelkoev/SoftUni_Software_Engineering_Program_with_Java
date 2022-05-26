0a85e7a44b5802f81e9d1680dc57bce960cbe23f
angelkoev <angel_koev@abv.bg>
2022-05-26 18:36:26 +0300

    Create test2.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E01_ZadachiOtIzpitite/test2.java
@@ -0,0 +1,11 @@
+public class test2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        double result = a / b;
+        System.out.println(result);
+
+        System.out.println(1 + 1 + "4" + 2 + 1);
+
+    }
+}
