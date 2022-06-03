58a18812bfd419751bbc19a9bbdf9ed8b022e7fc
angelkoev <angel_koev@abv.bg>
2022-06-03 21:35:27 +0300

    Create E03AnimalType.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E03_ConditionalStatementsAdvanced_Lab/E03AnimalType.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class E03AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String animal = scanner.nextLine();
+
+        switch (animal) {
+            case "dog":
+                System.out.println("mammal");
+                break;
+            case "crocodile":
+                System.out.println("reptile");
+                break;
+            case "tortoise":
+                System.out.println("reptile");
+                break;
+            case "snake":
+                System.out.println("reptile");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+
+
+
+        }
+
+
+    }
+}
