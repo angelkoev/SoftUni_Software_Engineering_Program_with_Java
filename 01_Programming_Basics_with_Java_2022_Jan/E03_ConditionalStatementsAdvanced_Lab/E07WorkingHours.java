99230a69fa4caa0f8c3dde436e90adfbcb68b4e9
angelkoev <angel_koev@abv.bg>
2022-06-04 18:13:38 +0300

    Create E07WorkingHours.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E03_ConditionalStatementsAdvanced_Lab/E07WorkingHours.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class E07WorkingHours {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hour = Integer.parseInt(scanner.nextLine());
+        String day = scanner.nextLine();
+
+        if (day.equals("Sunday")) {
+            System.out.println("closed");
+            } else {
+        if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday") || day.equals("Saturday"))
+                if (hour >= 10 && hour < 18) {
+                    System.out.println("open");
+                } else {
+                    System.out.println("closed");
+                }
+        }
+
+    }
+}
