2b0743f6982e325942822830f1b47a039314db50
angelkoev <angel_koev@abv.bg>
2022-06-15 19:07:16 +0300

    Create E01NumbersFrom1to100.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E04_ForLoop_Lab/E01NumbersFrom1to100.java
@@ -0,0 +1,11 @@
+public class E01NumbersFrom1to100 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
