808dfbb1613f7075a40d7022b89017e6c9ca503d
angelkoev <angel_koev@abv.bg>
2022-06-18 09:21:14 +0300

    Create E07SumNumbers.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E04_ForLoop_Lab/E07SumNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class E07SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum = sum + number;
+//            sum += number;
+        }
+        System.out.println(sum);
+    }
+}
