db28919c43bd02e093265495f50686ac715132c7
angelkoev <angel_koev@abv.bg>
2022-06-19 12:57:05 +0300

    Create E01NumbersEndingIn7.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E04_ForLoop_Exercise/E01NumbersEndingIn7.java
@@ -0,0 +1,16 @@
+public class E01NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+        }
+//        for (int i = 7; i <= 997; i +=10) {
+//            System.out.println(i);
+//                    }
+
+
+    }
+}
