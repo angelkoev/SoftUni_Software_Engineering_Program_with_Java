7f6b1517da1c2cf717de596909924139ceb0434e
angelkoev <angel_koev@abv.bg>
2022-06-22 19:56:22 +0300

    Create E09Clock.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E04_ForLoop_MoreExercise/E09Clock.java
@@ -0,0 +1,15 @@
+public class E09Clock {
+    public static void main(String[] args) {
+
+//        int hour = 0;
+//        int minutes = 0;
+
+        for (int i = 0; i <=23 ; i++) {
+            for (int j = 0; j <= 59; j++) {
+                System.out.printf("%d : %d%n", i, j);
+            }
+
+        }
+
+    }
+}
