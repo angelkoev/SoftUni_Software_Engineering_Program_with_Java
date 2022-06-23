4ddf0d027c4482d768961ef0c14ddac109c875b6
angelkoev <angel_koev@abv.bg>
2022-06-23 21:17:18 +0300

    Create E10Clock_Part2.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E04_ForLoop_MoreExercise/E10Clock_Part2.java
@@ -0,0 +1,20 @@
+public class E10Clock_Part2 {
+    public static void main(String[] args) {
+
+//        int hour = 0;
+//        int minutes = 0;
+//        int seconds = 0;
+
+        for (int i = 0; i <=23 ; i++) {
+            for (int j = 0; j <= 59; j++) {
+                for (int k = 0; k <=59 ; k++) {
+                    System.out.printf("%d : %d : %d%n", i, j, k);
+                }
+            }
+
+        }
+
+
+
+    }
+}
