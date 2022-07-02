ec8211b289aa0a17fce62079c6e9dcb04a6bc8df
angelkoev <angel_koev@abv.bg>
2022-07-02 16:38:28 +0300

    Create E07MinNumber.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E05_WhileLoop_Lab/E07MinNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class E07MinNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        String input = scanner.nextLine();
+
+        while (!input.equals("Stop")) {
+
+            int number = Integer.parseInt(input);
+            if (number < min) {
+                min = number;
+            }
+            input = scanner.nextLine();
+
+        }
+
+        System.out.println(min);
+
+
+    }
+}
