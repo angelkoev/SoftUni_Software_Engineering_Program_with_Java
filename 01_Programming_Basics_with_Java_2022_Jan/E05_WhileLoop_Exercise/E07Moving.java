e0ec3bbf67ee649afe41f12c063cdfe90942c2ca
angelkoev <angel_koev@abv.bg>
2022-07-04 22:59:05 +0300

    Create E07Moving.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E05_WhileLoop_Exercise/E07Moving.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class E07Moving {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int width = Integer.parseInt(scanner.nextLine());
+        int length = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+
+
+
+        int volume = width * length * height;
+
+//        int leftBoxes = 0;
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Done")) {
+
+            int boxes = Integer.parseInt(input);
+
+            volume = volume - boxes;
+
+            if (volume <= 0) {
+                break;
+            }
+
+            input = scanner.nextLine();
+
+        }
+
+        if (volume < 0) {
+            System.out.printf("No more free space! You need %d Cubic meters more.",Math.abs(volume));
+        } else {
+            System.out.printf("%d Cubic meters left.", volume);
+        }
+
+    }
+}
