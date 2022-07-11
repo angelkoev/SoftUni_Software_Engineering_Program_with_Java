4e4d5a6e6fa9007522efe1c95d2ea25a8833dd54
angelkoev <angel_koev@abv.bg>
2022-07-11 21:39:42 +0300

    Create E01Clock.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E06_NestedLoops_Lab/E01Clock.java
@@ -0,0 +1,19 @@
+public class E01Clock {
+    public static void main(String[] args) {
+
+//        i = tekusht chas
+//        j = tekushta minuta
+
+        for (int i = 0; i <= 23; i++) {
+            for (int j = 0; j <= 59; j++) {
+//                for (int k = 0; k <=59 ; k++) {
+//
+//                }
+                System.out.printf("%d:%d%n", i, j);
+//                System.out.println(i + ":" + j);
+            }
+        }
+
+
+    }
+}
