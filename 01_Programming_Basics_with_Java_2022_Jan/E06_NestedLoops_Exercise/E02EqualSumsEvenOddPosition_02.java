99e3e35e663a6cf9adfa2c03ad92018d1037ebdc
angelkoev <angel_koev@abv.bg>
2022-07-14 15:46:51 +0300

    Create E02EqualSumsEvenOddPosition_02.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E06_NestedLoops_Exercise/E02EqualSumsEvenOddPosition_02.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class E02EqualSumsEvenOddPosition_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        int secondNum = Integer.parseInt(scanner.nextLine());
+
+        for (int i = firstNum; i <= secondNum; i++) {
+            String currentNum = "" + i;
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int j = 0; j < 6; j++) {
+                int digit = Integer.parseInt("" + currentNum.charAt(j));
+
+                if (j % 2 == 0) {
+                    evenSum = evenSum + digit;
+                } else {
+                    oddSum = oddSum + digit;
+                }
+            }
+
+            if (evenSum == oddSum) {
+                System.out.print(i + " ");
+            }
+
+        }
+
+
+    }
+}
