41797e61e4228cc6fbdd86aafc46de0835fd6a8c
angelkoev <angel_koev@abv.bg>
2022-07-30 07:12:22 +0300

    Create E06MovieTickets.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/E06_ZadachiOtIzpitite/E06MovieTickets.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class E06MovieTickets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a1 = Integer.parseInt(scanner.nextLine());
+        int a2 = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = a1; i <= a2 - 1; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                for (int k = 1; k <= n / 2 - 1; k++) {
+
+                    if (i % 2 == 0) {
+                        continue;
+                    }
+
+                    if ((i + j + k) % 2 == 0) {
+                        continue;
+                    }
+
+                    System.out.printf("%c-%d%d%d%n", i, j, k, i);
+
+                }
+            }
+        }
+
+
+    }
+}
