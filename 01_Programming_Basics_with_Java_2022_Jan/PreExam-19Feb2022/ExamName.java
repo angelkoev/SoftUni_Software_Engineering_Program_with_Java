78fd53388d842a5de525d88e31705f9e3f8ecd22
angelkoev <angel_koev@abv.bg>
2022-08-08 18:46:33 +0300

    Create ExamName.java

---
+++ 01_Programming_Basics_with_Java_2022_Jan/PreExam-19Feb2022/ExamName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExamName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //  Programming Basics Online Pre - Exam - 19 and 20 February 2022.
+
+        // направих го за 44 мин.
+
+    }
+}
