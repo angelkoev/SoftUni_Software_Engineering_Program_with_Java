b06b68746f48fe0c53923b19710b934325697e47
angelkoev <angel_koev@abv.bg>
2022-08-10 20:05:48 +0300

    Create E03PassedOrFailed.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E01_BasicSyntaxConditionalStatementsAndLoops_Lab/E03PassedOrFailed.java
@@ -0,0 +1,17 @@
+package E01_BasicSyntaxConditionalStatementsAndLoops_Lab;
+
+import java.util.Scanner;
+
+public class E03PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+    }
+}
