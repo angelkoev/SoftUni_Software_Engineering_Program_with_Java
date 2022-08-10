f4272477659310324ae57e27f75b9ab603f859a5
angelkoev <angel_koev@abv.bg>
2022-08-10 20:05:14 +0300

    Create E01StudentInformation.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E01_BasicSyntaxConditionalStatementsAndLoops_Lab/E01StudentInformation.java
@@ -0,0 +1,18 @@
+package E01_BasicSyntaxConditionalStatementsAndLoops_Lab;
+
+import java.util.Scanner;
+
+public class E01StudentInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+
+
+
+    }
+}
