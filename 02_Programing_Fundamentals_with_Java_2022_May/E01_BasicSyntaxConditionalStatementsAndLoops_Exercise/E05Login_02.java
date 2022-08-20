8c897a9e6782bb661c520b87cd3b0c03dbd4625f
angelkoev <angel_koev@abv.bg>
2022-08-20 07:20:28 +0300

    Create E05Login_02.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E01_BasicSyntaxConditionalStatementsAndLoops_Exercise/E05Login_02.java
@@ -0,0 +1,34 @@
+package E01_BasicSyntaxConditionalStatementsAndLoops_Exercise;
+
+import java.util.Scanner;
+
+public class E05Login_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String username = scanner.nextLine();
+        String password = "";
+
+        for (int i = username.length() - 1; i >= 0; i--) {
+            password += username.charAt(i);
+//            password = password + username.charAt(i);
+        }
+        String input = scanner.nextLine();
+        int count = 0;
+        while (true) {
+            count++;
+            if (input.equals(password)) {
+                System.out.printf("User %s logged in.", username);
+                break;
+            } else {
+                if (count == 4) {
+                    System.out.printf("User %s blocked!", username);
+                    break;
+                }
+                System.out.println("Incorrect password. Try again.");
+            }
+           input = scanner.nextLine();
+        }
+
+    }
+}
