995f88956c3d411dc1a684861c0740e28503caa1
angelkoev <angel_koev@abv.bg>
2022-08-28 08:04:50 +0300

    Create E02PoundsToDollars.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E02_DataTypesAndVariables_Lab/E02PoundsToDollars.java
@@ -0,0 +1,13 @@
+package E02_DataTypesAndVariables_Lab;
+
+import java.util.Scanner;
+
+public class E02PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double pounds = Double.parseDouble(scanner.nextLine());
+        double dollars = pounds * 1.36;
+        System.out.printf("%.3f", dollars);
+    }
+}
