7b9dc1960eb9a504f896e6ca3bc9083673c1c807
angelkoev <angel_koev@abv.bg>
2022-09-15 00:24:11 +0300

    Create E01DataTypeFinder_02.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E02_DataTypesAndVariables_MoreExercise/E01DataTypeFinder_02.java
@@ -0,0 +1,27 @@
+package E02_DataTypesAndVariables_MoreExercise;
+
+import java.util.Scanner; // копирано решение от интернет
+
+public class E01DataTypeFinder_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String type = scanner.nextLine();
+
+        while (!type.equals("END")) {
+            Scanner input = new Scanner(type);
+            if (input.hasNextBoolean()) {
+                System.out.printf("%s is boolean type%n", type);
+            } else if (input.hasNextInt()) {
+                System.out.printf("%s is integer type%n", type);
+            } else if (type.length() == 1) {
+                System.out.printf("%s is character type%n", type);
+            } else if (input.hasNextDouble()) {
+                System.out.printf("%s is floating point type%n", type);
+            } else if (input.hasNextLine()) {
+                System.out.printf("%s is string type%n", type);
+            }
+            type = scanner.nextLine();
+        }
+    }
+}
