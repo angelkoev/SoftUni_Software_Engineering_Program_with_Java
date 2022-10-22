ced5d5721b817ac0801f462fee817f5b09cd85a3
Angel Koev <angel_koev@abv.bg>
2022-10-22 07:35:37 +0300

    Create E02VowelsCount.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E04_Methods_Exercise/E02VowelsCount.java
@@ -0,0 +1,25 @@
+package E04_Methods_Exercise;
+
+import java.util.Scanner;
+
+public class E02VowelsCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        printVowelsCount(text);
+
+    }
+
+    private static void printVowelsCount(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.toLowerCase().charAt(i);
+            if (symbol == 97 || symbol ==101 || symbol == 105 || symbol == 111 || symbol == 117 || symbol == 121) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
