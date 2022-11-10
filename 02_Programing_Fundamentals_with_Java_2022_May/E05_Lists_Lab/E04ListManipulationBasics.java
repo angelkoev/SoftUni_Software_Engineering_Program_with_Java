79013e15fd83f82f97cda53d07cd6fbd304b5e12
Angel Koev <angel_koev@abv.bg>
2022-11-10 17:59:06 +0200

    Create E04ListManipulationBasics.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E05_Lists_Lab/E04ListManipulationBasics.java
@@ -0,0 +1,51 @@
+package E05_Lists_Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class E04ListManipulationBasics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            String[] tokens = input.split(" ");
+
+            switch (tokens[0]) {
+                case "Add":
+                    int numberToAdd = Integer.parseInt(tokens[1]);
+                    numbers.add(numberToAdd);
+                    break;
+                case "Remove":
+                    int numberToRemove = Integer.parseInt(tokens[1]);
+                    numbers.remove(Integer.valueOf(numberToRemove));
+                    break;
+                case "RemoveAt":
+                    int indexToRemove = Integer.parseInt(tokens[1]);
+                    numbers.remove(indexToRemove);
+                    break;
+                case "Insert":
+                    int numberToInsert = Integer.parseInt(tokens[1]);
+                    int indexToInsert = Integer.parseInt(tokens[2]);
+                    numbers.add(indexToInsert, numberToInsert);
+                    break;
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+    }
+}
