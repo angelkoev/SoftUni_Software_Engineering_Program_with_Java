e91f4c4d05c9dd351a4b5e1377d89d81cef484f2
Angel Koev <angel_koev@abv.bg>
2022-11-17 17:40:32 +0200

    Create E06CardsGame.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E05_Lists_Exercise/E06CardsGame.java
@@ -0,0 +1,52 @@
+package E05_Lists_Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class E06CardsGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> firstPlayerCards = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> secondPlayerCards = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+
+        while (firstPlayerCards.size() != 0 && secondPlayerCards.size() != 0) {
+
+            int firstCard = firstPlayerCards.get(0);
+            int secondCard = secondPlayerCards.get(0);
+
+            firstPlayerCards.remove(0);
+            secondPlayerCards.remove(0);
+
+            if (firstCard > secondCard) {
+                firstPlayerCards.add(firstCard);
+                firstPlayerCards.add(secondCard);
+            } else if (secondCard > firstCard) {
+                secondPlayerCards.add(secondCard);
+                secondPlayerCards.add(firstCard);
+            }
+        }
+
+        if (firstPlayerCards.size() == 0) {
+            System.out.printf("Second player wins! Sum: %d", getCardsSum(secondPlayerCards));
+        } else if (secondPlayerCards.size() == 0) {
+            System.out.printf("First player wins! Sum: %d", getCardsSum(firstPlayerCards));
+        }
+
+
+    }
+
+    private static int getCardsSum(List<Integer> listCards) {
+        int sum = 0;
+        for (Integer card : listCards) {
+            sum += card;
+        }
+        return sum;
+    }
+}
+
