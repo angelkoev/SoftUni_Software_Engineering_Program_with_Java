d85b0764285475b19a7e9c7d386c89fcdd2b29b0
Angel Koev <angel_koev@abv.bg>
2022-11-23 17:52:25 +0200

    Create SlivaneNaDvaSpisukaKoitoSaSortirani.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E05_Lists_Lab/SlivaneNaDvaSpisukaKoitoSaSortirani.java
@@ -0,0 +1,39 @@
+package E05_Lists_Lab;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class SlivaneNaDvaSpisukaKoitoSaSortirani {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> sortedA = new ArrayList<>(Arrays.asList(10, 6, 3, 2, 1));
+        List<Integer> sortedB = new ArrayList<>(Arrays.asList(9, 8, 4, 0));
+
+        int indexA = 0;
+        int indexB = 0;
+
+        List<Integer> merged = new ArrayList<>();
+
+        while (indexA < sortedA.size() || indexB < sortedB.size()) {
+
+            if (indexA < sortedA.size() && sortedA.get(indexA) > sortedB.get(indexB)) {
+                merged.add(sortedA.get(indexA));
+                indexA++;
+            } else {
+                merged.add(sortedB.get(indexB));
+                indexB++;
+            }
+
+        }
+
+
+        for (int number : merged) {
+            System.out.print(number + " ");
+        }
+
+
+    }
+}
