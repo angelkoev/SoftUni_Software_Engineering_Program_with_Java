625cdec7008cf60c824aabdd8eab0e886eaa2bb7
Angel Koev <angel_koev@abv.bg>
2023-03-10 23:07:18 +0200

    Create E04TeamworkProjects_ZaReshavane.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E06_ObjectsAndClasses_MoreExercise/E04TeamworkProjects_ZaReshavane.java
@@ -0,0 +1,27 @@
+package E06_ObjectsAndClasses_MoreExercise;
+
+import java.util.Scanner;
+
+// оставям я за по-натам
+
+public class E04TeamworkProjects_ZaReshavane {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String inuptData = scanner.nextLine();
+            String user = inuptData.split(" ")[0];
+            String team = inuptData.split(" ")[1];
+        }
+
+//
+//        while () {
+//            // TODO:
+//        }
+
+    }
+
+
+}
