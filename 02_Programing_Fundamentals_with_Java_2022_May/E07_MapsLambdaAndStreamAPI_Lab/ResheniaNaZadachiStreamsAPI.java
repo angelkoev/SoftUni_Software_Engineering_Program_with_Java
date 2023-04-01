9a18e5dde17e9e9c1cc0e10add92a812f3959bff
Angel Koev <angel_koev@abv.bg>
2023-04-01 20:56:49 +0300

    Create ResheniaNaZadachiStreamsAPI.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E07_MapsLambdaAndStreamAPI_Lab/ResheniaNaZadachiStreamsAPI.java
@@ -0,0 +1,5 @@
+package E07_MapsLambdaAndStreamAPI_Lab;
+
+// https://softuni.bg/forum/18220/streams-api-exercises-resheniya-na-zadachite-s-izpolzvane-na-striymove
+public class ResheniaNaZadachiStreamsAPI {
+}
