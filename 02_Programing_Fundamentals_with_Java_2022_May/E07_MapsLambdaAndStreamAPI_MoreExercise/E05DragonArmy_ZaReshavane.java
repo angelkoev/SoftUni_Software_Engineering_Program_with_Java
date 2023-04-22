25e58ef0f01a17d7664b5f1a39169c0a53f8a7b0
Angel Koev <angel_koev@abv.bg>
2023-04-22 18:38:32 +0300

    Create E05DragonArmy_ZaReshavane.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E07_MapsLambdaAndStreamAPI_MoreExercise/E05DragonArmy_ZaReshavane.java
@@ -0,0 +1,11 @@
+package E07_MapsLambdaAndStreamAPI_MoreExercise;
+
+import java.util.Scanner;
+
+public class E05DragonArmy_ZaReshavane {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
