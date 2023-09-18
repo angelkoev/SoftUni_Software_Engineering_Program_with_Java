870e1cb39851681537dcc7f557bf605959280535
Angel Koev <angel_koev@abv.bg>
2023-09-18 20:37:57 +0300

    Create E03PlantDiscovery_DimitarRachev.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E10_FinalExamRetake02/E03PlantDiscovery_DimitarRachev.java
@@ -0,0 +1,80 @@
+package E10_FinalExamRetake02;
+
+        import java.util.*;
+
+public class E03PlantDiscovery_DimitarRachev {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Plant> plants = new LinkedHashMap<>();
+        int n = Integer.parseInt(scanner.nextLine());
+        while (n-- > 0) {
+            String[] input = scanner.nextLine().split("<->");
+            String name = input[0];
+            int rarity = Integer.parseInt(input[1]);
+            plants.put(name, new Plant(name, rarity));
+        }
+
+        String line;
+        while (!"Exhibition".equals(line = scanner.nextLine())) {
+            String[] input = line.split(":\\s+");
+            String command = input[0];
+            String[] plantInfo = input[1].split(" - ");
+            String plant = plantInfo[0];
+            if (!plants.containsKey(plant)) {
+                System.out.println("error");
+
+            } else {
+                switch (command) {
+                    case "Rate":
+                        Double rating = Double.parseDouble(plantInfo[1]);
+                        plants.get(plant).getRatings().add(rating);
+                        break;
+                    case "Update":
+                        int rarity = Integer.parseInt(plantInfo[1]);
+                        plants.get(plant).setRarity(rarity);
+                        break;
+                    case "Reset":
+                        plants.get(plant).resetRatings();
+                        break;
+                }
+            }
+        }
+
+        System.out.println("Plants for the exhibition:");
+        plants.values().forEach(System.out::println);
+    }
+
+    static class Plant {
+        String name;
+        int rarity;
+        List<Double> ratings;
+
+        public Plant(String name, int rarity) {
+            this.name = name;
+            this.rarity = rarity;
+            this.ratings = new ArrayList<>();
+        }
+
+        private Double getAverageRating() {
+            return this.ratings.stream().mapToDouble(e -> e).average().orElse(0);
+        }
+
+        public void setRarity(int rarity) {
+            this.rarity = rarity;
+        }
+
+        public List<Double> getRatings() {
+            return ratings;
+        }
+
+        public void resetRatings() {
+            this.ratings = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("- %s; Rarity: %d; Rating: %.2f", this.name, this.rarity, getAverageRating());
+        }
+    }
+}
