f83a5697a62f9f2a2d9413c86bbebc8644c4598b
Angel Koev <angel_koev@abv.bg>
2023-10-24 18:35:49 +0300

    Create E02FancyBarcodes_DimitarRachev.java

---
+++ 02_Programing_Fundamentals_with_Java_2022_May/E10_FinalExamRetake04/E02FancyBarcodes_DimitarRachev.java
@@ -0,0 +1,35 @@
+package E10_FinalExamRetake04;
+
+        import java.util.Scanner;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
+
+public class E02FancyBarcodes_DimitarRachev {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Pattern pattern = Pattern.compile("@#+(?<item>[A-Z][A-Za-z0-9]{4,}[A-Z])@#+");
+        int n = Integer.parseInt(scanner.nextLine());
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                String item = matcher.group("item");
+                Pattern digitpattern = Pattern.compile("\\d+");
+                Matcher digitMatcher = digitpattern.matcher(item);
+                StringBuilder groupId = new StringBuilder();
+                while (digitMatcher.find()) {
+                    groupId.append(digitMatcher.group());
+                }
+                if (groupId.length() == 0) {
+                    System.out.println("Product group: 00");
+                } else {
+                    System.out.println("Product group: " + groupId);
+                }
+            } else {
+                System.out.println("Invalid barcode");
+            }
+        }
+    }
+}
